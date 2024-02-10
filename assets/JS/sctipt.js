2944456b2fe148bd870de47528babe02ac72b3af
Crystal D <crystaldepalma@yahoo.com>
2024-02-10 13:57:39 -0500

    completed getDadJoke functionality and reflected it in HTML - working properly with the same eventListener as WeatherAPI

--- assets/JS/sctipt.js
+++ assets/JS/sctipt.js
@@ -20,62 +20,46 @@ document.getElementById('weatherForm').addEventListener('submit', function(event
       const weatherInfoDiv = document.getElementById('weatherInfo');
       weatherInfoDiv.innerHTML = `
         <h2>Current Weather</h2>
-        <p>Location: ${weatherData.name}</p>
+        <p>Location: ${
+weatherData.name
+}</p>
         <p>Temperature: ${weatherData.main.temp}°F</p>
-        <p>Weather: ${weatherData.weather[0].description}</p>
+        <p>Weather: ${
+weatherData.weather
+[0].description}</p>
       `;
+
+      // Call function to get dad joke
+      getDadJoke();
     })
     .catch(error => {
       console.error('Error:', error);
     });
 });
 
-$(document).ready(function() {
-    // API Query Paramaters
-    var baseUrl = 'https://api-ninjas.com/api/dadjokes';
-    var API = "rEJQDvZhoRkHeGUuNv/8Lw==wykGgqxEBWyA81NE";
+function getDadJoke() {
+  fetch('https://api.api-ninjas.com/v1/dadjokes?limit=1', {
+    headers: {
+      'X-Api-Key': 'rEJQDvZhoRkHeGUuNv/8Lw==wykGgqxEBWyA81NE'
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    // Get the dad joke text
+    const jokeText = data[0].joke;
 
-    // init local storage array holding the variable
-    var jokes = [];
-    var limit = 1; 
-    
-    $('#getJokeButton').click(function() { 
-        getJoke();
-    }); 
-
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/dadjokes?limit=1' + limit, 
-        headers: { 'X-Api-Key': 'rEJQDvZhoRkHeGUuNv/8Lw==wykGgqxEBWyA81NE'},
-        contentType: 'application/json',
-        success: function(result) {
-            console.log(result);
-        },
-        error: function ajaxError(jqXHR) {
-            console.error('Error: ', jqXHR.responseText);
-        }
-    });
-
-})
-
-
-// *** HOW TO HAVE MULTIPLE FUNCTIONS PER EVENT-LISTENER BUTTON *** //
-// Function 1
-// function function1() {
-//     console.log("Function 1 executed");
-//   }
+    // Display the dad joke on the page
+    const dadJokeDiv = document.getElementById('dadJoke');
+    dadJokeDiv.innerHTML = `
+      <h2>Dad Joke</h2>
+      <p>${jokeText}</p>
+    `;
+  })
   
-  // Function 2
-//   function function2() {
-//     console.log("Function 2 executed");
-//   }
-  
-  // New function that calls both functions
-//   function renderTwoFunctions() {
-//     function1();
-//     function2();
-//   }
-  
-  // Assign the new function as the event listener
-//   const button = document.getElementById("myButton");
-//   button.addEventListener("click", renderTwoFunctions);
+  .catch(error => {
+    console.error('Error:', error);
+  });
+} 
+
+
+
