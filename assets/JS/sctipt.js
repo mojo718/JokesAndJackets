f3829e69457cf9c5e453df29b20bf7ebf9e711f2
Chelsea <chibby@chelseas-mbp.mynetworksettings.com>
2024-02-10 16:08:04 -0500

    Added Attire Functionality

--- assets/JS/sctipt.js
+++ assets/JS/sctipt.js
@@ -20,14 +20,13 @@ document.getElementById('weatherForm').addEventListener('submit', function(event
       const weatherInfoDiv = document.getElementById('weatherInfo');
       weatherInfoDiv.innerHTML = `
         <h2>Current Weather</h2>
-        <p>Location: ${
-weatherData.name
-}</p>
+        <p>Location: ${weatherData.name}</p>
         <p>Temperature: ${weatherData.main.temp}°F</p>
-        <p>Weather: ${
-weatherData.weather
-[0].description}</p>
+        <p>Weather: ${weatherData.weather[0].description}</p>
       `;
+
+      // Display attire suggestion based on weather condition
+      displayAttireSuggestion(weatherData.weather[0].description);
 
       // Call function to get dad joke
       getDadJoke();
@@ -55,60 +54,48 @@ function getDadJoke() {
       <p>${jokeText}</p>
     `;
   })
-  
   .catch(error => {
     console.error('Error:', error);
   });
-} 
+}
 
-//THIS IS COMMENTED OUT UNTIL WE HAVE UP AND RUNNING!!!
-// function getGif()
-// // needs to call file folder: GIF
+// Map weather conditions to attire suggestions
+const attireSuggestions = {
+  'clear sky': 'Wear light clothing and sunglasses.',
+  'few clouds': 'You might want to bring a light jacket or sweater.',
+  'scattered clouds': 'Consider bringing an umbrella just in case.',
+  'broken clouds': 'Wear layers for variable weather conditions.',
+  'shower rain': 'Bring an umbrella and wear waterproof shoes.',
+  'rain': 'Don\'t forget your raincoat and waterproof boots.',
+  'thunderstorm': 'Stay indoors if possible. If not, dress for rain and wind.',
+  'snow': 'Bundle up with a warm coat, gloves, and boots.',
+  'mist': 'Wear layers and a light waterproof jacket.',
+  'smoke': 'Avoid prolonged outdoor activities and wear a mask if needed.',
+  'haze': 'Consider wearing a mask or scarf to protect your airways.',
+  'fog': 'Be cautious while driving and wear reflective clothing if necessary.',
+  'dust': 'Wear a mask and protect your eyes and airways.',
+  'sand': 'Cover exposed skin and wear goggles to protect your eyes.',
+  'ash': 'Wear a mask and protect your eyes and airways from ash particles.',
+  'squall': 'Stay indoors if possible. If not, dress for strong winds and rain.',
+  'tornado': 'Seek shelter immediately and avoid windows. Dress for safety and warmth.',
+  'volcanic ash': 'Wear a mask and protect your eyes and airways from ash particles.',
+  'overcast clouds': 'Wear layers and be prepared for potentially cool temperatures.'
+  // Add more conditions and attire suggestions as needed
+};
 
-
-// {
-//   // research how to call img folder from desktop in JS within API
-//   const getGif=gif[0].gif
-// }
-// // Display the gif
-// const gif = document.getElementById(getGif);
-
-
-
-
-// function displayRandomGif() {
-//   // Array of URLs for your GIFs
-//   const gifUrls = [
-//     //PATH TO IMAGES IN PROJECT FOLDER GOES HERE - NO UNDERSCORES IN GIF NAMES, PLEASE, IE GIF1, GIF2//
-//     'url_to_gif_1',
-//     'url_to_gif_2',
-//     'url_to_gif_3',
-//     // Add more URLs as needed
-//   ];
-
-
-//   // Get a random index within the range of the gifUrls array
-//   const randomIndex = Math.floor(Math.random() * gifUrls.length);
-
-
-// //h2 element to call in the front:
-
-
-// { <h2>getGif</h2>
-// <p>$(getGif)</p> }
-
-
-//   // Get the random GIF URL
-//   const randomGifUrl = gifUrls[randomIndex];
-
-
-//   // Display the GIF on the page
-//   const gifDiv = document.getElementById('jokeGif');
-//   gifDiv.innerHTML = `
-//     <img src="${randomGifUrl}" alt="Random GIF">
-//   `;
-// }
-
+// Display attire suggestion based on weather condition
+function displayAttireSuggestion(weatherCondition) {
+  const attireSuggestion = attireSuggestions[weatherCondition.toLowerCase()];
+  if (attireSuggestion) {
+    const weatherConditionDiv = document.getElementById('weatherCondition');
+    weatherConditionDiv.innerHTML = `
+      <h2>Attire Suggestion</h2>
+      <p>${attireSuggestion}</p>
+    `;
+  } else {
+    console.log('No attire suggestion available for this weather condition.');
+  }
+};
 
 //ATTIRE FUNCTION
 // needs to be tested : weather objects from json API call need to be reviewed and listed below, and then put div id tag on html page, ie div id=""
